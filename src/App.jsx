01001b2d691a81b80cc7624eb1c0e1cc344e4fcf
Maxim <maxaabbakz@gmail.com>
2023-10-17 07:35:22 +0600

    added form, prlist

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import { useEffect } from 'react';
 import './App.css';
 import { useTelegram } from './hooks/useTelegram';
 import Header from './components/Header/Header';
+import { Route, Routes } from 'react-router-dom';
+import ProductList from './components/ProductList/ProductList';
+import Forms from './components/Forms/Form';
 function App() {
   const {tg, onToggleButton} = useTelegram();
 
@@ -11,7 +14,10 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <button onClick={onToggleButton}>toggle</button>
+      <Routes>
+        <Route index element={<ProductList />} />
+        <Route path='/form' element={<Form />} />
+      </Routes>
     </div>
   );
 }
