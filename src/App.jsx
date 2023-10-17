c4d3bbe3063513f98ba1e458b69e494d0558392e
Maxim <maxaabbakz@gmail.com>
2023-10-17 07:53:48 +0600

    fix FORMMM

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
       <Header />
       <Routes>
         <Route index element={<ProductList />} />
-        <Route path='form' element={<Form />} />
+        <Route path='/form' element={<Form />} />
       </Routes>
     </div>
   );
