7cd921550d4e86345219bb8578cb229a06afcae5
Maxim <maxaabbakz@gmail.com>
2023-10-17 08:03:07 +0600

    hfgj

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './Form.css';
+import { useTelegram } from '../../hooks/useTelegram';
 const Form = () => {
 	const [country, setCountry] = useState('');
 	const [street, setStreet] = useState('');
 	const [subject, setSubject] = useState('physical');
+	const { tg } = useTelegram();
+
+	useEffect(() => {
+		tg.MainButton.setParams({
+			text: 'Отправить данные',
+		});
+	}, []);
+	useEffect(() => {
+		if (!street || !country) {
+			tg.MainButton.hide();
+		} else {
+			tg.MainButton.show();
+		}
+	}, [country, street]);
+
 	const onChangeCountry = (e) => {
 		setCountry(e.target.value);
 	};
