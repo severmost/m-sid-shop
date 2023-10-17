5461cff713e8de4718d2a33c14601a8d96c8b1f1
Maxim <maxaabbakz@gmail.com>
2023-10-17 07:49:47 +0600

    link form

--- src/components/Forms/Form.jsx
+++ src/components/Forms/Form.jsx
@@ -1,11 +1,29 @@
 import React from 'react';
-import './Form.css'
+import './Form.css';
 const Form = () => {
-    return (
-        <div>
-            Form
-        </div>
-    );
+	const [country, setCountry] = useState('');
+	const [street, setStreet] = useState('');
+	const [subject, setSubject] = useState('physical');
+	const onChangeCountry = (e) => {
+		setCountry(e.target.value);
+	};
+	const onChangeStreet = (e) => {
+		setStreet(e.target.value);
+	};
+	const onChangeSubject = (e) => {
+		setSubject(e.target.value);
+	};
+	return (
+		<div className="form">
+			<h3>Введите ваши данные</h3>
+			<input onChange={onChangeCountry} value={country} type="text" className="input" placeholder="Страна" />
+			<input onChange={onChangeStreet} value={street} type="text" className="input" placeholder="Улица" />
+			<select onChange={onChangeSubject} value={subject} className="select">
+				<option value="physical">Физ. лицо</option>
+				<option value="legal">Юр. лицо</option>
+			</select>
+		</div>
+	);
 };
 
 export default Form;
